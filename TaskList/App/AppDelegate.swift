1e53494f86f5a7e3a9df43888834de6361d659b5
Neyilanau <halapovalbek@gmail.com>
2025-10-06 00:43:52 +0300

    UPD

---
+++ TaskList/App/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  TaskList
+//
+//  Created by Альбек Халапов on 28.09.2025.
+//
+
+import UIKit
+
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    private var storageManager = StorageManager.shared
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = UINavigationController(rootViewController: TaskListViewController())
+        return true
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        storageManager.saveContext()
+    }
+
+}
+
---
+++ TaskList/App/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  TaskList
+//
+//  Created by Альбек Халапов on 28.09.2025.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+    private var storageManager = StorageManager.shared
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = UINavigationController(rootViewController: TaskListViewController())
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        storageManager.saveContext()
+    }
+}
+
